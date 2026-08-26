9907c282e601c4553f22e64630a546ca9453f980
izzy2611 <nrzt0740@leeds.ac.uk>
2026-08-26 21:02:43 +0000

    external file

--- banking-system.c
+++ banking-system.c
@@ -10,27 +10,42 @@ int main(void) {
     char main_menu;
 
     struct account_details accounts[100];
+    FILE *fptr;
+    fptr = fopen("Accounts.txt", "w");
+    fprintf(fptr, "Name: Account Number:");
     printf("Would you like to go to the menu? Y/N");
-    scanf("%c", &main_menu);
-    while (main_menu == "Y") {
+    scanf(" %c", &main_menu);
+    while (main_menu == 'Y') {
         printf("Please pick an option:\n");
         printf("1. Create new account\n");
 
-        scanf("%d", &user_choice);
+        scanf(" %d", &user_choice);
 
         if (user_choice == 1) {
+
             printf("Please enter your name:\n");
-            scanf("%s", accounts[num_of_accounts].name);
+            scanf(" %s", accounts[num_of_accounts].name);
 
             printf("Please create a pin number:\n");
-            scanf("%s", accounts[num_of_accounts].pin_number);  
+            scanf(" %s", accounts[num_of_accounts].pin_number);  
 
-            printf("Your account number is: %d", num_of_accounts); 
-            printf("Your balance is: %d", balance);
+            fptr = fopen("Accounts.txt", "r");
+            
+
+            printf("Your account number is: %d",  num_of_accounts); 
+            printf("Your balance is: %d",  balance);
+
+            fprintf(fptr, "%s %d \n", accounts[num_of_accounts].name, num_of_accounts);
 
             num_of_accounts++;
-            printf("Would you like to go back to the menu? Y/N");
-            scanf("%c", &main_menu);
+            printf("Would you like to go back to the menu? Y/N\n");
+            scanf(" %c", &main_menu);
+            
+        //else if(user_choice == 2) {
+
+
+
+        //}
         }
     }
 
