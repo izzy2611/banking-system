77d98e47ebb5cde2ad2f242c21af2f57775d1320
izzy2611 <nrzt0740@leeds.ac.uk>
2026-08-27 20:18:26 +0000

    3 options

--- banking-system.c
+++ banking-system.c
@@ -8,16 +8,21 @@ int main(void) {
     int user_choice;
     int balance = 0;
     char main_menu;
+    int user_account;
+    int transfer_amount;
+    int i;
 
     struct account_details accounts[100];
     FILE *fptr;
     fptr = fopen("Accounts.txt", "w");
-    fprintf(fptr, "Name: Account Number:");
+    fprintf(fptr, "Name:   Account Number:\n");
     printf("Would you like to go to the menu? Y/N");
     scanf(" %c", &main_menu);
     while (main_menu == 'Y') {
         printf("Please pick an option:\n");
         printf("1. Create new account\n");
+        printf("2. Transfer money\n");
+        printf("3. View balance\n");
 
         scanf(" %d", &user_choice);
 
@@ -29,25 +34,50 @@ int main(void) {
             printf("Please create a pin number:\n");
             scanf(" %s", accounts[num_of_accounts].pin_number);  
 
-            fptr = fopen("Accounts.txt", "r");
+            //fptr = fopen("Accounts.txt", "r");
             
 
-            printf("Your account number is: %d",  num_of_accounts); 
-            printf("Your balance is: %d",  balance);
+            printf("Your account number is: %d\n",  num_of_accounts); 
+            printf("Your balance is: %d\n",  balance);
 
-            fprintf(fptr, "%s %d \n", accounts[num_of_accounts].name, num_of_accounts);
-
+            fprintf(fptr, " %s %d %d\n", accounts[num_of_accounts].name, num_of_accounts, accounts[num_of_accounts].balance);
+        
             num_of_accounts++;
             printf("Would you like to go back to the menu? Y/N\n");
             scanf(" %c", &main_menu);
+    }
+        else if(user_choice == 2) {
+            printf("Please enter your account number:\n");
+            scanf(" %d", &user_account);
+            printf("How much would you like to transfer?");
+            scanf(" %d", &transfer_amount);
             
-        //else if(user_choice == 2) {
+            for (i = 0; i < num_of_accounts; i++){
+                if (accounts[user_account].account_number == accounts[i].account_number){
+                    fprintf(fptr, "%d ", accounts[i].balance += transfer_amount);
 
+                }   
+            }
+            printf("Would you like to go back to the menu? Y/N\n");
+            scanf(" %c", &main_menu);
+    }
+        
+        //view balance
+        else if (user_choice == 3) {
+            printf("What is your account number?");
+            scanf(" %d", &user_account);
+            for (i = 0; i < num_of_accounts; i++){
+                if (accounts[user_account].account_number == accounts[i].account_number){
+                    fptr = fopen("Accounts.txt", "r");
+                    fprintf(fptr, "%d ", accounts[i].balance);
+                }
 
-
-        //}
         }
     }
-
+    printf("Would you like to go back to the menu? Y/N\n");
+            scanf(" %c", &main_menu);
+    }
+        
     return 0;
 }
+
